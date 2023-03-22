c3f1ca6a357aeed72102f9ac811587ecb7a035d2
bot4o <98636639+bot4o@users.noreply.github.com>
2023-03-22 16:10:48 +0200

    16:10

---
+++ WebApplication7/Controllers/CategoryController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting.Internal;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Xml.Schema;
+using WebApplication7.Data;
+using WebApplication7.Models;
+
+namespace WebApplication7.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            this._db = db;
+        }
+        public IActionResult Index()
+        {
+            var objlist = _db.Category.ToList();// IEnumerable<Category> 
+            return View(objlist);
+        }
+        public IActionResult Create()
+        {
+            //GET - CREATE
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            //POST - CREATE
+            _db.Category.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}

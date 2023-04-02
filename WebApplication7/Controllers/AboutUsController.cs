e7f4a2c8804f0c9c21d02043bea1e32dbb9197e0
Aleksandar Boev <boevaleksandar@gmail.com>
2023-04-03 00:13:58 +0300

    Aboutus made in chatgpt UwU

---
+++ WebApplication7/Controllers/AboutUsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication7.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication7/Models/AboutUs.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace WebApplication7.Models
+{
+    public class AboutUs
+    {
+
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+    }
+}

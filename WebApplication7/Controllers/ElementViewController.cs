4b5c2c8bcd3c7f5b0f8dde53ce82a38c963df07b
Aleksandar Boev <boevaleksandar@gmail.com>
2023-04-05 11:44:54 +0300

    made page for the main elements

---
+++ WebApplication7/Controllers/ElementViewController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication7.Controllers
+{
+    public class ElementViewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication7/Models/ElementView.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace WebApplication7.Models
+{
+    public class ElementView
+    {
+
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+    }
+}
